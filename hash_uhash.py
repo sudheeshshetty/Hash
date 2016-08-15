d83b86bfed81d180f5c9918d2582be60392a95e9
Sudheesh Shetty <sudheeshshetty@gmail.com>
2016-08-15 17:55:24 +0530

    COde for hashing and un hashing the string

---
+++ hash_uhash.py
@@ -0,0 +1,23 @@
+def hash(s):
+    h = 7
+    letters = "acdegilmnoprstuw"
+    for i in range(0,len(s)):
+        h = (h * 37 + letters.index(s[i]))
+    return h
+print hash("leepadg")
+
+def uhash(number):
+    letters = "acdegilmnoprstuw"
+    s=""
+    while number>7:
+        if number%37==0:
+            s=letters[0]+s
+        else:
+            for i in range(0,len(letters)):
+                if (number-i)%37==0:
+                    s=letters[i]+s
+                    break
+        number=number/37
+    return s
+        
+print uhash(680131659347)
