a66567111811640fc6a7767b42770ef36bbb356a
Sudheesh Shetty <sudheeshshetty@gmail.com>
2016-08-15 18:04:37 +0530

    Add comments

--- hash_uhash.py
+++ hash_uhash.py
@@ -7,12 +7,15 @@ def hash(s):
 print hash("lawnmower")
 
 def uhash(number):
+    #Characters that can be used
     letters = "acdegilmnoprstuw"
     s=""
     while number>7:
+        #To check if it is the first character in 'letters'
         if number%37==0:
             s=letters[0]+s
         else:
+            #find the next character of string
             for i in range(0,len(letters)):
                 if (number-i)%37==0:
                     s=letters[i]+s
