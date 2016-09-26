962bb56d12298b82ffa3882d48c2c7861d98b77c
Sudheeshshetty <sudheeshshetty@gmail.com>
2016-09-26 09:15:28 +0530

    made it flexible to additional characters.
    made changes so that it can be used for almost all characters.

--- hash_uhash.py
+++ hash_uhash.py
@@ -1,14 +1,13 @@
 def hash(s):
     h = 7
-    letters = "acdegilmnoprstuw"
+    letters = "abcdefghijklmnopqrstuvwxyz-_1234567890@!#$%&*.,"
     for i in range(0,len(s)):
         h = (h * 37 + letters.index(s[i]))
     return h
-print hash("lawnmower")
 
-def uhash(number):
+def unhash(number):
     #Characters that can be used
-    letters = "acdegilmnoprstuw"
+    letters = "abcdefghijklmnopqrstuvwxyz-_1234567890@!#$%&*.,"
     s=""
     while number>7:
         #To check if it is the first character in 'letters'
@@ -22,5 +21,3 @@ def uhash(number):
                     break
         number=number/37
     return s
-        
-print uhash(930846109532517)
